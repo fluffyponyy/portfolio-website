5e64da1963f393c4d86cee4942abfebe736780a7
fluffyponyy <emilymooshroom@gmail.com>
2025-11-18 19:53:28 -0700

    social media buttons

--- src/components/links.tsx
+++ src/components/links.tsx
@@ -1,14 +1,28 @@
 'use client';
 import './nav.css';
+import Image from "@/components/image";
 
 export default function Links() {
 
     return (
         <nav className="link-container">
             <div className="link-buttons">
-                <a className="sm-link" href=""></a>
-                <a className="sm-link" href=""></a>
-                <a className="sm-link" href=""></a>
+                <a className="sm-link" href="https://www.linkedin.com/in/ana-constantinescu-ab3046198/">
+                    <Image key={1}
+                        url={"linkedin.png"}
+                        height={48}
+                        width={48}></Image></a>
+                <a className="sm-link" href="https://github.com/fluffyponyy">
+                    <Image key={2}
+                        url={"github.png"}
+                        height={48}
+                        width={48}></Image>
+                </a>
+                <a className="sm-link" href="">
+                    <Image key={3}
+                        url={"email.png"}
+                        height={48}
+                        width={48}></Image></a>
             </div >
         </nav >
     );
