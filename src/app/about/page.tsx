e5fb8133681c03ec1168a3a08dc45404e8c67fae
fluffyponyy <emilymooshroom@gmail.com>
2025-11-18 19:53:50 -0700

    adding social media

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,16 +4,20 @@ import ProjectCard from "@/components/project_card";
 import './about.css';
 import ProjectModal from "@/components/project_modal";
 import Nav from "@/components/nav";
+import Links from "@/components/links"
 
 
 export default function AboutPage() {
 
     return (
-        <div className="projects-container">
-            <div className="projects-header">
+        <div className="about-container">
+            <div className="about-header">
                 <h1>about</h1>
                 <div className="nav-links">
                     <Nav />
+                </div>
+                <div className="about-body">
+                    <Links></Links>
                 </div>
             </div>
 
