c443b49e24453738e83fe4c0503839e7e337c339
fluffyponyy <emilymooshroom@gmail.com>
2025-11-25 20:59:54 -0700

    circle option

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -7,14 +7,16 @@ type Image = {
     url: string;
     height: number;
     width: number;
+    shape?: string;
 }
 
-export default function Image({ url, height, width }: Image) {
+export default function Image({ url, height, width, shape }: Image) {
     return (
 
         <img
             src={`/assets/images/${url}`} // prepend public folder path
             style={{ height: `${height}px`, width: `${width}px` }}
+            className={shape ? "circle" : ""}
         />
 
     )
