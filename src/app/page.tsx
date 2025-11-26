a3349f70cee9e3a76df39c35b954f3b2ee2b05fd
fluffyponyy <emilymooshroom@gmail.com>
2025-11-25 20:58:55 -0700

    revamp first page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,26 @@
 'use client';
 import './page.css';
 import Nav from "@/components/nav"
+import Links from "@/components/links"
+import StarField from "@/components/star_field_wrapper";
+import { useEffect } from 'react';
 
 export default function Home() {
+    useEffect(() => {
+        document.title = "Home | Ana Constantinescu";
+    }, []);
 
 
     return (
-        <div className="container">
-            <div className="star">⭐</div>
-            <h1 className="name">Ana Constantinescu</h1>
-            <h2>Computer Science student at UVic</h2>
-            < Nav />
-        </div>
+        <>
+            <StarField count={80} />
+            <div className="container   main">
+                <div className="shooting-star">⭐</div>
+                <h1 className="name">Ana Constantinescu</h1>
+                <h2>Computer Science student at UVic</h2>
+                < Nav />
+                <Links />
+            </div>
+        </>
     );
 }
