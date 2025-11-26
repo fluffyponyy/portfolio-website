e0c4c7a6b9a2a703726306a5c2e24399c9290b4f
fluffyponyy <emilymooshroom@gmail.com>
2025-11-25 20:59:31 -0700

    rework

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,17 +1,19 @@
 'use client';
-import { useState, useEffect } from "react";
-import ProjectCard from "@/components/project_card";
 import './about.css';
-import ProjectModal from "@/components/project_modal";
 import Nav from "@/components/nav";
 import Links from "@/components/links"
 import Image from "@/components/image";
+import StarField from "@/components/star_field_wrapper";
+import { useEffect } from 'react';
 
 export default function AboutPage() {
+    useEffect(() => {
+        document.title = "About | Ana Constantinescu";
+    }, []);
 
     return (
-        <div className="about-container">
-            <div className="about-header">
+        <div className="primary-container">
+            <div className="primary-header">
                 <h1>about</h1>
                 <div className="nav-links">
                     <Nav />
@@ -19,18 +21,21 @@ export default function AboutPage() {
 
             </div>
 
-            <div className="about-body">
-                <h2>Hey! I'm Ana 💫</h2>
+            <div className="page-body">
+                <StarField count={80} />
+                <h1>Hey! I'm Ana 💫</h1>
 
                 <Image key={1}
                     url={"its_me.jpeg"}
-                    height={36}
-                    width={48}></Image>
+                    height={400}
+                    width={500}
+                    shape="circle"
+                ></Image>
 
                 <h3> I'm a 4th year Computer Science student at the University of Victoria in BC.</h3>
 
                 <p>Reach out to me on social media:</p>
-                <Links></Links>
+                <Links />
             </div>
 
 
